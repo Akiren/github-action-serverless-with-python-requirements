ca4602d15e57591b330423002534f01597d11f5c
Lorenzo Lee <renrock@gmail.com>
2020-03-20 11:30:49 -0500

    Update index.js
    Removed sudo from config and deploy options

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ async function installServerlessAndPlugins() {
 async function runServerlessDeploy() {
   await exeq(
     `echo Running sudo sls deploy ${ARGS}...`,
-    `sudo sls config credentials --provider aws --key ${AWS_ACCESS_KEY_ID} --secret ${AWS_SECRET_ACCESS_KEY} ${ARGS}`,
-    `sudo sls deploy ${ARGS}`
+    `serverless config credentials --provider aws --key ${AWS_ACCESS_KEY_ID} --secret ${AWS_SECRET_ACCESS_KEY} ${ARGS}`,
+    `serverless deploy ${ARGS}`
   )
 }
 
